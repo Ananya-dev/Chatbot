88ffaf363ba59d2de013275a049770db4159d30c
Ananya-dev <64248612+Ananya-dev@users.noreply.github.com>
2020-06-22 18:53:13 +0530

    Create words.py

---
+++ words.py
@@ -0,0 +1,13 @@
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+
+        # take each word and tokenize it
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        # adding documents
+        documents.append((w, intent['tag']))
+
+        # adding classes to our class list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
